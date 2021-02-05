21057f11fbb05312a523b4fe6d3bafecba7a2a2f
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2021-02-05 20:42:52 +0530

    Update Day5(Trapping_water).py

--- Day5(Trapping_water).py
+++ Day5(Trapping_water).py
@@ -11,8 +11,7 @@
 # Input: arr[]   = {3, 0, 2, 0, 4}
 # Output: 7
 # Explanation:
-# We can trap "3 units" of water between 3 and 2,
-# "1 unit" on top of bar 2 and "3 units" between 2 and 4.  See below diagram also.
+# We can trap "3 units" of water between 3 and 2,"1 unit" on top of bar 2 and "3 units" between 2 and 4. 
 
 # ***************************************************************************************************
 
