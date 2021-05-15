04325989075a039530ff03b81fc749f3ca42c29b
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2021-05-15 23:28:14 +0530

    Add files via upload

---
+++ Day103(Spiral_level_order).cpp
@@ -0,0 +1,36 @@
+/*
+Write a function to print spiral order traversal of a tree. 
+*/
+
+// **********************************************************************************************************
+
+vector<int> findSpiral(Node *root)
+{
+    stack <Node *> stk1;
+    stack <Node *> stk2;
+    vector <int> vec;
+    if (root == NULL) return vec;
+    stk1.push(root);
+    while (!stk1.empty() || !stk2.empty()){
+        
+        while (!stk1.empty()){
+            Node *temp = stk1.top();
+            vec.push_back(temp->data);
+            stk1.pop();
+            
+            if (temp->right) stk2.push(temp->right);
+            if (temp->left) stk2.push(temp->left);
+        }
+        
+        while (!stk1.empty()){
+            Node *temp = stk2.top();
+            vec.push_back(temp->data);
+            stk2.pop();
+            
+            if (temp->left) stk1.push(temp->left);
+            if (temp->right) stk1.push(temp->right);
+        }
+    }
+    
+    return vec;
+}
